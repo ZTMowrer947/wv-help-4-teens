b92f626d0f10a6218773b35ff72026bad9bd0624
ZTMowrer947 <Zackmowrer@gmail.com>
2019-09-28 11:48:27 -0400

    Add logger, views and static middleware to app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
 // Imports
+import path from "path";
 import Koa from "koa";
+import logger from "koa-logger";
+import mount from "koa-mount";
+import serveStatic from "koa-static";
+import views from "koa-views";
 
 // App setup
 const app = new Koa();
+
+// Middleware
+app.use(logger());
+app.use(mount("/public", serveStatic(path.resolve("..", "public"))));
+app.use(
+    views(path.resolve("..", "views"), {
+        extension: "pug",
+    })
+);
 
 // Export
 export default app;
