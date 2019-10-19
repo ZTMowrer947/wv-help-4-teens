e9ead33b58d5f9c4f15db03cf53a18328a50ed54
Zackery Mowrer <Zackmowrer@gmail.com>
2019-10-19 12:51:04 -0400

    Add build task to gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+// Imports
+const gulp = require("gulp");
+const sm = require("gulp-sourcemaps");
+const babel = require("gulp-babel");
+const webpack = require("webpack");
+const webpackStream = require("webpack-stream");
+const webpackConfig = require("./webpack.config");
+
+// Paths
+const paths = {
+    server: ["src/server/**/*.ts", "src/*.ts"],
+    client: ["src/client/index.ts"],
+};
+
+// Tasks
+const buildServer = () => {
+    return gulp
+        .src(paths.server, { base: "src" })
+        .pipe(sm.init())
+        .pipe(babel())
+        .pipe(sm.write("."))
+        .pipe(gulp.dest("dist"));
+};
+
+const buildClient = () => {
+    return gulp
+        .src(paths.client)
+        .pipe(sm.init())
+        .pipe(webpackStream(webpackConfig, webpack))
+        .pipe(sm.write("."))
+        .pipe(gulp.dest("public"));
+};
+
+const build = gulp.parallel(buildServer, buildClient);
+
+// Exports
+module.exports = {
+    build,
+};
