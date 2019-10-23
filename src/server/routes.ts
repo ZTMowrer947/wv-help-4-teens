e292255a928dd9ecd2449e07d216c13a3f3843f7
Zackery Mowrer <Zackmowrer@gmail.com>
2019-10-23 12:45:53 -0400

    Attach topic service to middleware

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,12 +1,27 @@
 // Imports
+import { ParameterizedContext } from "koa";
 import Router from "koa-router";
+import BaseState from "./models/BaseState";
+import TopicService from "./services/TopicService";
 
 // Router setup
 const router = new Router();
 
+// Middleware
+router.use(async (ctx: ParameterizedContext<BaseState>, next) => {
+    // Initialize topic service
+    ctx.state.topicService = new TopicService();
+
+    // Get topic listing
+    ctx.state.topics = await ctx.state.topicService.getTopics();
+
+    // Continue middleware chain
+    await next();
+});
+
 // Routes
 // GET /: Home page
-router.get("/", async ctx => {
+router.get("/", async (ctx: ParameterizedContext<BaseState>) => {
     // Render home page
     await ctx.render("index");
 });
