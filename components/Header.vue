3a61529d0a9fa171b9ffc0474a7a08e5b2c0f8ee
Zackery Mowrer <Zackmowrer@gmail.com>
2020-01-25 16:10:33 -0500

    Add topic dropdown to header component

--- components/Header.vue
+++ components/Header.vue
@@ -7,6 +7,15 @@
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav class="mr-auto">
                     <b-nav-item to="/">Home</b-nav-item>
+                    <b-nav-item-dropdown text="Topics" right>
+                        <b-dropdown-item
+                            v-for="topic of topics"
+                            :key="topic.id"
+                            :to="`/topics/${topic.id}`"
+                        >
+                            {{ topic.name }}
+                        </b-dropdown-item>
+                    </b-nav-item-dropdown>
                 </b-navbar-nav>
             </b-collapse>
         </b-navbar>
@@ -15,8 +24,22 @@
 
 <script lang="ts">
 // Imports
-import Vue from 'vue';
+import { Component, Vue } from 'vue-property-decorator';
+import { Topic } from '@/data';
+import TopicService from '@/services/TopicService';
 
 // Component
-export default Vue.extend({});
+@Component({})
+class Header extends Vue {
+    topics: Topic[] = [];
+
+    mounted() {
+        new TopicService().getList().then(recievedTopic => {
+            this.topics = recievedTopic;
+        });
+    }
+}
+
+// Export
+export default Header;
 </script>
