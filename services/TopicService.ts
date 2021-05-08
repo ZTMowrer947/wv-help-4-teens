a3d00e8e6f5ec2c1878aff929486dd50e065cc24
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2021-05-08 08:36:15 -0400

    Update TopicService to compile again

--- services/TopicService.ts
+++ services/TopicService.ts
@@ -3,17 +3,17 @@ import topicData, { Topic } from '../data';
 
 // Service
 export default class TopicService {
-    private topicData: typeof topicData;
+    #topicData: typeof topicData;
 
-    public constructor() {
-        this.topicData = topicData;
+    constructor() {
+        this.#topicData = topicData;
     }
 
-    public async getList(): Promise<Topic[]> {
-        return this.topicData;
+    async getList(): Promise<Topic[]> {
+        return this.#topicData;
     }
 
-    public async getById(id: string): Promise<Topic | undefined> {
-        return this.topicData.find(topic => topic.id === id);
+    async getById(id: string): Promise<Topic | undefined> {
+        return this.#topicData.find(topic => topic.id === id);
     }
 }
